2177951c29fd2f61f0c643e704565bfbbf4b4414
IbneRaveem <raveem80@gmail.com>
2023-06-16 10:20:33 +0530

    changes in html file

--- BS/settings.py
+++ BS/settings.py
@@ -96,6 +96,12 @@ DATABASES = {
 #         'HOST': 'localhost'
 #     }
 # }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
