2d3f8cb4204eec88c2b5cd1f007e2426e0b56c50
Guda Vineeth Reddy <109956056+VineethReeddyBAAIML@users.noreply.github.com>
2023-06-19 11:41:27 +0530

    Update settings.py

--- BS/settings.py
+++ BS/settings.py
@@ -167,6 +167,7 @@ EMAIL_HOST_PASSWORD = 'pspcqvroxtycnlif'
 EMAIL_USE_TLS = True
 
 # CRISPY_TEMPLATE_PACK = 'uni_form'
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 CSRF_TRUSTED_ORIGINS=["https://woxsen-campus-mart-production.up.railway.app"] 
 CORS_ALLOWED_ORIGINS = [
     'https://woxsen-campus-mart-production.up.railway.app',
