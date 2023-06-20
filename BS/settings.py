499d2481f3faf1ce2ce7382fdca95263a060d07c
Guda Vineeth Reddy <109956056+VineethReeddyBAAIML@users.noreply.github.com>
2023-06-20 09:55:03 +0530

    Update settings.py

--- BS/settings.py
+++ BS/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATABASE_URL ="postgresql://postgres:qsMMlC6tw3uLD4CZ9t6T@containers-us-west-10.railway.app:7064/railway"
+DATABASE_URL ="postgresql://postgres:Tu3DreS41UVWbNnjV7fs@containers-us-west-174.railway.app:7763/railway"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
